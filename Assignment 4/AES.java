c1697de38048f63a7ff15ceca7144919126675b7
dannyqdvd <dannyqdvd@yahoo.com>
2015-07-01 12:08:53 -0500

    Update AES.java
    Added instructions on how to code key schedule

--- Assignment 4/AES.java
+++ Assignment 4/AES.java
@@ -30,14 +30,54 @@ public class AES{
 		String plaintext = "00112233445566778899AABBCCDDEEFF";
 		String cipherKey = "0000000000000000000000000000000000000000000000000000000000000000"
 
-
 		/*End Debugging........................................*/
+
+
+
+
+		//do key expansion for 256 bit key
+		//print key expansion
+		//do it 14 times
+			
+			//rotword()
+				// -take last column of round
+				// 	-first item in column goes to the end, everything shifts up
+				// -subbyte each item with s box
+				// -XOR it with each column in the key round
+
+				// -returns column in new round
+
+
+
+
+
+		////////////Encrpytion/////////////////////
+
+		//initial round - add round key
+
+		//13 rounds 
+		///subbytes
+		//shift rowa
+		//mix columns
+		//add round key
+
+		//final round
+		//subbytes
+		//shiftrows
+		//addroundkey
+
+		//
+
+
 
 
 		//decrypting file 
 		//You'll read in a line, converting from Hex to binary for storage into your state array. 
 		//Apply the AES algorithm to encrypt the string as stored, and 
 		//write out the ciphertext in Hex notation to the output file. 
+
+
+
 
 
 
